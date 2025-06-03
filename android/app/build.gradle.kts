53022581765870b70c36fbcdeb72c5ffd9baef0c
Krishnam Agarwal <888krishnam@gmail.com>
2025-06-03 13:59:38 +0530

    intialize firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.test_flutter"
+    namespace = "com.example.frontend"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.13113456"
 
